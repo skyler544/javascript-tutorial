1ef4f62b4c82ce7e8cadeb3ae3cb41007ce828fc
Skyler Mayfield <skyler544@gmail.com>
2022-05-31 15:22:12 +0200

    added drawing functions

--- game.js
+++ game.js
@@ -1,2 +1,27 @@
-var canvas = document.getElementById("gameWorld");
-var ctx = canvas.getContext("2d");
+// wait until the page ist loaded in order to start the game
+document.addEventListener("DOMContentLoaded", function (event) {
+  var canvas = document.getElementById("gameWorld");
+  var ctx = canvas.getContext("2d");
+
+  drawShape(ctx, circle);
+  drawShape(ctx, rectangle);
+});
+
+
+let drawShape = function (ctx, drawFunction) {
+  ctx.beginPath();
+  drawFunction(ctx);
+  ctx.closePath();
+}
+
+let rectangle = function (ctx) {
+  ctx.rect(20, 40, 50, 50);
+  ctx.fillStyle = "#FF0000";
+  ctx.fill();
+}
+
+let circle = function (ctx) {
+  ctx.arc(240, 160, 20, 0, Math.PI * 2, false);
+  ctx.fillStyle = "green";
+  ctx.fill();
+}
