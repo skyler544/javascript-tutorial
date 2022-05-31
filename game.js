c9c096d54e3bc1e981179c8add8e0b2073bbf7b3
Skyler Mayfield <skyler544@gmail.com>
2022-05-31 15:36:28 +0200

    abstract functions more

--- game.js
+++ game.js
@@ -3,25 +3,31 @@ document.addEventListener("DOMContentLoaded", function (event) {
   var canvas = document.getElementById("gameWorld");
   var ctx = canvas.getContext("2d");
 
-  drawShape(ctx, circle);
-  drawShape(ctx, rectangle);
+  drawShape(ctx, 130, 130, 20, 0, circle);
+  drawShape(ctx, 30, 40, 50, 20, outlineRectangle);
 });
 
 
-let drawShape = function (ctx, drawFunction) {
+let drawShape = function (ctx, posX, posY, sizeX, sizeY, drawFunction) {
   ctx.beginPath();
-  drawFunction(ctx);
+  drawFunction(ctx, posX, posY, sizeX, sizeY);
   ctx.closePath();
 }
 
-let rectangle = function (ctx) {
-  ctx.rect(20, 40, 50, 50);
+let rectangle = function (ctx, posX, posY, sizeX, sizeY) {
+  ctx.rect(posX, posY, sizeX, sizeY);
   ctx.fillStyle = "#FF0000";
   ctx.fill();
 }
 
-let circle = function (ctx) {
-  ctx.arc(240, 160, 20, 0, Math.PI * 2, false);
+let outlineRectangle = function (ctx, posX, posY, sizeX, sizeY) {
+  ctx.rect(posX, posY, sizeX, sizeY);
+  ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
+  ctx.stroke();
+}
+
+let circle = function (ctx, posX, posY, radius, startAngle) {
+  ctx.arc(posX, posY, radius, startAngle, Math.PI * 2, false);
   ctx.fillStyle = "green";
   ctx.fill();
 }
